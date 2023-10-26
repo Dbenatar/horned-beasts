c183c60a8dec8c55f573e3d9b9c102ea5bc3aa01
Dom <dom.benatar@hotmail.com>
2023-10-26 15:17:31 +0100

    form started

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -10,21 +10,39 @@ function App() {
 const [showModal, setShowModal] = useState(false);
 const [shownBeast, setShownBeast] = useState({});
 
+// form, dropdown menu to view favourites
+const [form, setForm] = useState ({
+  horns: "",
+})
+
+function handleSubmit(event) {
+  event.preventDefault();
+}
+
   function handleShowModal(beast) {
     setShowModal(!showModal);
     setShownBeast(beast);
   }
 
+function handleChange(event) {
+  setForm({ ...form, [event.target.name]: event.taget.value});
+}
+
   return (
-    <>
+    <div>
       <Header />
+      <form onSubmit={handleSubmit}>
+        <label>Number of horns</label>
+        <input type="number" />
+        <button>Submit</button>
+      </form>
       <Gallery data={data} handleShowModal={handleShowModal} />
       <Footer />
       {showModal && (
         <SelectedBeast shownBeast={shownBeast}
         handleShowModal={handleShowModal} />
       )}
-    </>
+    </div>
   )
 }
 
